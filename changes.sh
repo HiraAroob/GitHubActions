6689e65fe0082fdf2db7334f00e42bd2e8113222
HiraAroob <hira.aroob@abacuscambridge.com>
2021-11-11 12:22:11 +0500

    updated

--- changes.sh
+++ changes.sh
@@ -3,6 +3,8 @@ for dir in */; do
 	ConfigChanges=`git diff --name-only HEAD HEAD~1 | grep "$dir"`
 	#echo "$ConfigChanges"
 	#echo "$?"
+	git reflog
+	#git checkout HEAD@{...}
 	if [[ $ConfigChanges = "" ]]
 	then
 		echo "No organization changes detected"
@@ -11,8 +13,8 @@ for dir in */; do
 		my_array=($(echo $ConfigChanges))
 			
 			for i in "${my_array[@]}"; do   # access each element of array
-					
-				mvn -Ptest apigee-config:apps -Dusername=hira.aroob@abacuscambridge.com -Dpassword=  -Dapigee.config.file=$i -Dapigee.config.options=update 				
+				echo "$i"	
+				#mvn -Ptest apigee-config:apps -Dapigee.config.file=$i -Dapigee.config.options=update 				
 			done
 		echo "update maven command"
 	fi
